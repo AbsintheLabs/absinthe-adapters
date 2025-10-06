465895cae76d78343f638d3718f7ba172a442a20
Andrew Magid <magid.andrew@gmail.com>
2025-10-06 11:22:38 -0500

    Added ESLint configuration for adapter files ignore them before we migrate away from them

--- eslint.config.js
+++ eslint.config.js
@@ -11,6 +11,7 @@ export default [
       'projects/*/src/model/generated/**',
       'projects/*/db/migrations/**',
       'projects/*/abi/**',
+      'src/adapters/**',
       'vitest.config.ts',
       'projects',
     ],
