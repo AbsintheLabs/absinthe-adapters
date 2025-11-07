a772c6f479d22eabefe90c93f1eba510d7a5d5c7
Kushagra Sharma <skushagra.sharma@gmail.com>
2025-11-08 00:11:11 +0530

    add: monad-support-old-arch-printr

--- projects/printr/src/utils/consts.ts
+++ projects/printr/src/utils/consts.ts
@@ -15,6 +15,7 @@ const CHAIN_BASE_TOKENS = [
   }, //AVAX on Avalanche
   { address: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2', name: 'WETH', coingeckoId: 'ethereum' }, //WETH on Ethereum
   { address: '0x82af49447d8a07e3bd95bd0d56f35241523fbab1', name: 'WETH', coingeckoId: 'ethereum' }, //WETH on Arbitrum
+  { address: '0x3bd359c1119da7da1d913d1c4d2b7c461115433a', name: 'WMON', coingeckoId: 'monad' }, //WMON on Monad
 ];
 
 export { LIQUIDITY_FEE, LIQUIDITY_FEE_BSC, CHAIN_BASE_TOKENS };
